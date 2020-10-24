7c15341d2bdd955743f69247540347741faa81d5
NIC619 <twedusuck@gmail.com>
2020-10-25 00:39:17 +0800

    disable exec silent

--- cli/test/utils.ts
+++ cli/test/utils.ts
@@ -1,7 +1,7 @@
 import * as shell from 'shelljs'
 
 const exec = (command: string) => {
-    return shell.exec(command, { silent: true })
+    return shell.exec(command, { silent: false })
 }
 
 export { exec }
